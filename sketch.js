416f6a734e086f73a8f6e5c4c19033faa8cd2f96
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2019-07-07 17:35:17 +1000

    The birds now have a brain, need to implement the genetic algorithm next.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 let pipes = [];
+let pipePopulation = 5;
 let PIPE_SPEED = 4;
+
+let birds = [];
+let POPULATION_SIZE = 100;
 
 let GRAVITY = 0.3;
 
@@ -8,9 +12,11 @@ let score = 0;
 function setup() {
     createCanvas(1200, 600);
 
-    bird = new Bird;
+    for (let i = 0; i < POPULATION_SIZE; i++) {
+        birds[i] = new Bird;
+    }
 
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < pipePopulation; i++) {
         pipes[i] = new Pipe(width + (250 * i));
     }
 
@@ -20,41 +26,53 @@ function setup() {
 function draw() {
     background(0);
 
+    fill(255);
+    rect(pipes[closestPipe()].x, 0, pipes[closestPipe()].width, pipes[closestPipe()].top);
+    rect(pipes[closestPipe()].x, pipes[closestPipe()].bottom, pipes[closestPipe()].width, height - (pipes[closestPipe()].top - 150));
+    noFill();
 
-    bird.show();
-    bird.move();
+    for (let i = 0; i < POPULATION_SIZE; i++) {
+        birds[i].show();
+        //bird.think();
+        if (birds[i].think() > 0.5) {
+            birds[i].applyForce();
+        }
+        birds[i].move();
+    }
 
-    for (var i = 0; i < 5; i++) {
-        pipes[i].show();
-        pipes[i].move();
+    for (let j = 0; j < pipePopulation; j++) {
+        pipes[j].show();
+        pipes[j].move();
     }
 
     var hit = false;
     var hit1 = false;
     //var score = 0;
 
-    for (var i = 0; i < 5; i++) {
-        hit = collideRectCircle(pipes[i].x, 0, pipes[i].width, pipes[i].top, bird.x, bird.y, bird.diameter);
-        hit1 = collideRectCircle(pipes[i].x, pipes[i].bottom, pipes[i].width, height - (this.top - 150), bird.x, bird.y, bird.diameter);
+    for (var k = 0; k < pipePopulation; k++) {
+        hit = collideRectCircle(pipes[k].x, 0, pipes[k].width, pipes[k].top, birds[k].x, birds[k].y, birds[k].diameter);
+        hit1 = collideRectCircle(pipes[k].x, pipes[k].bottom, pipes[k].width, height - (this.top - 150), birds[k].x, birds[k].y, birds[k].diameter);
 
         if (hit || hit1) {
-            noLoop();
+            //noLoop();
         }
     }
 
     //console.log(score);
-    rectMode(CENTER);
-    text(score, bird.x-3, bird.y);
-    rectMode(CORNER);
+    // rectMode(CENTER);
+    // text(score, birds[i].x - 3, birds[i].y);
+    // rectMode(CORNER);
+
 
 }
 
-function keyPressed() {
-    if (key == ' ') {
-        bird.velocity -= 8;
+// function keyPressed() {
+//     if (key == ' ') {
+//         bird.velocity -= 8;
 
-    }
-}
+//     }
+// }
+
 
 class Bird {
     constructor() {
@@ -63,7 +81,36 @@ class Bird {
         this.acceleration = GRAVITY;
         this.velocity = 0;
         this.diameter = 35;
+        this.brain = new NeuralNetwork(5, 10, 1);
+
     }
+
+    applyForce() {
+        this.velocity -= 8;
+    }
+
+    think() {
+        let inputs = [];
+        // inputs[0] = this.y;
+        // inputs[1] = this.velocity;
+        // inputs[2] = pipes.top;
+        // inputs[3] = pipes.bottom;
+        // inputs[4] = pipes.x;
+        //let closestPipe = pipes.closestX();
+
+        inputs[0] = this.y/height;
+        inputs[1] = this.velocity;
+        inputs[2] = pipes[closestPipe()].x/width;
+        inputs[3] = pipes[closestPipe()].top/height;
+        inputs[4] = pipes[closestPipe()].bottom/height;
+
+        let outputs = this.brain.predict(inputs);
+
+        return outputs;
+
+    }
+
+
 
     show() {
         stroke(255);
@@ -76,7 +123,7 @@ class Bird {
             this.y = height;
             this.velocity = 0;
         }
-        else if(this.y < 0){
+        else if (this.y < 0) {
             this.y = 0;
             this.velocity = 0;
         }
@@ -90,6 +137,21 @@ class Bird {
             this.y += this.velocity;
         }
     }
+}
+
+let closestPipe = function () {
+    let closestX = Infinity;
+    let closestPipe = 0;
+    for (let i = 0; i < pipePopulation; i++) {
+        if (pipes[i].passed == false) {
+            if (pipes[i].x < closestX) {
+                closestPipe = i;
+                closestX = pipes[i].x;
+            }
+        }
+    }
+    //console.log("debug");
+    return closestPipe;
 }
 
 class Pipe {
@@ -109,7 +171,7 @@ class Pipe {
     move() {
         this.x -= PIPE_SPEED;
 
-        if(this.x < bird.x && this.passed == false){
+        if (this.x < birds[0].x - (this.width * 1.5) && this.passed == false) {
             score++;
             this.passed = true;
         }
@@ -121,4 +183,16 @@ class Pipe {
             this.passed = false;
         }
     }
+
+    // closestX() {
+    //     let closestX = Infinity;
+    //     for (let i = 0; i < pipes.length(); i++) {
+    //         if (pipes[i].passed == false) {
+    //             if (pipes[i].x < closestX) {
+    //                 closestX = pipes[i].x;
+    //             }
+    //         }
+    //     }
+    //     return closestX;
+    // }
 }
